cc55eedc7a52538031845118aa337ee3aed85ac1
Kamal <kamala@gmail.com>
2026-02-25 07:57:03 +0700

    add mysql2 dependency

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,14 +1,15 @@
 import { NextResponse } from "next/server";
 import { db } from "@/lib/db";
+import { RowDataPacket } from "mysql2";
 
 export async function POST(request: Request) {
   const body = await request.json();
   const { email, password } = body;
 
-  const [rows]: any = await db.query(
-    "SELECT * FROM admins WHERE email = ? AND password = ?",
-    [email, password]
-  );
+ const [rows] = await db.query<RowDataPacket[]>(
+  "SELECT * FROM admins WHERE email = ? AND password = ?",
+  [email, password]
+);
 
   if (rows.length > 0) {
     return NextResponse.json({ success: true });
